7fdffe6ed0f0bbf906d2cfa2d04fa0ebbabfc744
Lincoln de Sousa <lincoln@comum.org>
2011-11-03 12:27:45 -0200

    Substituindo um "include" por "include_once"

--- wpgd.admin.php
+++ wpgd.admin.php
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-include('wpgd.templating.php');
+include_once('wpgd.templating.php');
 
 $renderer = new WpGdTemplatingRenderer();
 
