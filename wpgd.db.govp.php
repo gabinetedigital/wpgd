d0e0cbc9bcd6dd3fd31ca2841057cb8ac041418e
Thiago Silva <thiago@metareload.com>
2011-12-01 15:19:00 -0200

    Adicionando wpgd_db_get_unique_contribs()

--- wpgd.db.govp.php
+++ wpgd.db.govp.php
@@ -15,21 +15,27 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-function wpgd_db_get_contribs($sortby, $page, $perpage, $theme, $status, $s) {
+function wpgd_db_get_contribs($sortby,
+                              $page,
+                              $perpage,
+                              $theme,
+                              $status,
+                              $s,
+                              $filter = null) {
     $page = (strlen($page) == 0) ?  '0' : $page;
     $offset = $page * $perpage;
     $sortfields = array(
-        'id' => 'c.id' ,
-        'status' => 'c.status',
-        'theme' => 'c.theme',
-        'date'  => 'c.creation_date',
-        'author' => 'u.display_name',
-        'title' => 'c.title'
+        'id' => 'contrib.id' ,
+        'status' => 'contrib.status',
+        'theme' => 'contrib.theme',
+        'date'  => 'contrib.creation_date',
+        'author' => 'user.display_name',
+        'title' => 'contrib.title'
     );
     if (isset($sortfields[$sortby])) {
         $sortfield = $sortfields[$sortby];
     } else {
-        $sortfield = 'c.id';
+        $sortfield = 'contrib.id';
     }
 
     function index_of($arr, $id) {
@@ -44,16 +50,16 @@ function wpgd_db_get_contribs($sortby, $page, $perpage, $theme, $status, $s) {
     }
 
     /* Handling filters */
-    $filter = "";
+    $filter = $filter ? " AND ($filter) " : "";
     $filters = array();
     if (!empty($theme)) {
-        array_push($filters, "c.theme = '$theme'");
+        array_push($filters, "contrib.theme = '$theme'");
     }
     if (!empty($status)) {
-        array_push($filters, "c.status = $status");
+        array_push($filters, "contrib.status = $status");
     }
     if (count($filters) > 0) {
-        $filter = "AND (" . join(" AND ", $filters) . ")";
+        $filter .= "AND (" . join(" AND ", $filters) . ")";
     }
 
     /* Handling the text search */
@@ -66,20 +72,21 @@ function wpgd_db_get_contribs($sortby, $page, $perpage, $theme, $status, $s) {
     global $wpdb;
     $sql_head = "
       SELECT
-          c.id, c.title, c.content, c.creation_date, c.theme, c.original,
-          c.status, u.display_name, c.parent, c.part, c.moderation, u.ID as user_id
+          contrib.id, contrib.title, contrib.content, contrib.creation_date,
+          contrib.theme, contrib.original, contrib.status, user.display_name,
+          contrib.parent, contrib.part, contrib.moderation, user.ID as user_id
     ";
 
     $sql_base ="
       FROM
-          contrib c, wp_users u
+          contrib, wp_users user
       WHERE
-          (c.user_id=u.ID AND c.enabled=1) $filter $search
+          (contrib.user_id=user.ID AND contrib.enabled=1) $filter $search
       ORDER BY $sortfield
     ";
 
     $sql_main = $sql_head . $sql_base . "LIMIT $offset, $perpage";
-    $sql_count = "SELECT COUNT(c.id) AS total " . $sql_base;
+    $sql_count = "SELECT COUNT(contrib.id) AS total " . $sql_base;
 
     if (mysql_client_encoding($wpdb->dbh) == 'utf8') {
         mysql_set_charset("latin1", $wpdb->dbh);
@@ -89,6 +96,22 @@ function wpgd_db_get_contribs($sortby, $page, $perpage, $theme, $status, $s) {
     mysql_set_charset("utf8", $wpdb->dbh);
 
     return array($results, $count);
+}
+
+function wpgd_db_get_unique_contribs($sortby
+                                     , $page
+                                     , $perpage
+                                     , $theme
+                                     , $status
+                                     , $s) {
+
+    return wpgd_db_get_contribs($sortby
+                                , $page
+                                , $perpage
+                                , $theme
+                                , $status
+                                , $s
+                                , "contrib.parent=0");
 }
 
 
